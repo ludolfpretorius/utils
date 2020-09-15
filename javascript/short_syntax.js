3b7394552e1fe2587c9f19c07f7afcd6f8b99133
Ludolf Pretorius <48451857+ludolfpretorius@users.noreply.github.com>
2020-09-15 09:43:07 +0200

    Update short_syntax.js

--- javascript/short_syntax.js
+++ javascript/short_syntax.js
@@ -8,3 +8,4 @@ Object.prototype.el = function(el, where = document) {
 el('#element') // returns single HTML element
 el('div') // returns array of elements
 el('div', el('#element')) // Optional second parameter to specify where to look for the element(s).
+
